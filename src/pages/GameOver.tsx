c60ccc5cf4ce5752340cbc50ba19f4de033f1e23
takebon <ytakeshi_7@yahoo.co.jp>
2025-02-06 00:55:38 +0900

    九九実装

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import { useSoundManager } from "../components/SoundManager";
 import { useNavigate } from "react-router-dom";
 import SoundButton from "../components/SoundButton";
