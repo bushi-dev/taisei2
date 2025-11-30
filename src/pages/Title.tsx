ad7b5d73179cdd781d7017ad541a2681e144e25d
bushi-dev <ytakeshi_7@yahoo.co.jp>
2025-11-30 11:43:01 +0900

    設定を追加

--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
-import { useSoundManager } from "../components/SoundManager";
-import { useNavigate } from "react-router-dom";
-import SoundButton from "../components/SoundButton";
-import "./Title.css";
-import { getPath } from "../util/util";
+import { useEffect } from 'react';
+import { useSoundManager } from '../components/SoundManager';
+import { useNavigate } from 'react-router-dom';
+import SoundButton from '../components/SoundButton';
+import './Title.css';
+import { getPath } from '../util/util';
 
 const Title = () => {
   const navigate = useNavigate();
@@ -12,25 +12,22 @@ const Title = () => {
 
   useEffect(() => {
     // BGM再生
-    playBgm("/sound/bgm1.mp3", 0.1);
+    playBgm('/sound/bgm1.mp3', 0.1);
   }, [playBgm]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(
-          "https://my-cloudflare-worker.ytakeshi-7.workers.dev/list",
-          {
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        console.log("Response:", res);
+        const res = await fetch('https://my-cloudflare-worker.ytakeshi-7.workers.dev/list', {
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        console.log('Response:', res);
         const text = await res.text();
-        console.log("Text:", text);
-        localStorage.setItem("treasures", text);
+        console.log('Text:', text);
+        localStorage.setItem('treasures', text);
       } catch (error) {
         console.error(error);
       }
@@ -41,39 +38,26 @@ const Title = () => {
   return (
     <div className="title-screen">
       <h1 className="title-screen__heading">たいせい忍者</h1>
-      <img
-        src={getPath("/image/nin.png")}
-        alt="ninja"
-        className="title-screen__ninja"
-      />
+      <img src={getPath('/image/nin.png')} alt="ninja" className="title-screen__ninja" />
 
-      <SoundButton
-        className="title-screen__button start"
-        onClick={() => navigate("/level")}
-      >
+      <SoundButton className="title-screen__button start" onClick={() => navigate('/level')}>
         冒険を始める
       </SoundButton>
 
-      <SoundButton
-        className="title-screen__button kuku"
-        onClick={() => navigate("/kuku-level")}
-      >
+      <SoundButton className="title-screen__button kuku" onClick={() => navigate('/kuku-level')}>
         九九モード
       </SoundButton>
 
-      {window.location.href.includes("local") && (
+      {window.location.href.includes('local') && (
         <SoundButton
           className="title-screen__button kokugo"
-          onClick={() => navigate("/kokugo-level")}
+          onClick={() => navigate('/kokugo-level')}
         >
           国語モード
         </SoundButton>
       )}
 
-      <SoundButton
-        className="title-screen__button treasure"
-        onClick={() => navigate("/treasure")}
-      >
+      <SoundButton className="title-screen__button treasure" onClick={() => navigate('/treasure')}>
         秘宝の記録
       </SoundButton>
     </div>
