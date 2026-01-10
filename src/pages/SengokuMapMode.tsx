a254b527bc988ef2a6bf25f11d3cd5d379ef1166
takeshi <ytakeshi_7@yahoo.co.jp>
2026-01-10 10:41:19 +0900

    update

--- src/pages/SengokuMapMode.tsx
+++ src/pages/SengokuMapMode.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import JapanMap from '../components/JapanMap';
 import SoundButton from '../components/SoundButton';
